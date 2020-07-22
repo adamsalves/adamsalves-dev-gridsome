6626346e888496a653561e569bc92cf114445f9e
Adams Alves <alves.adams@gmail.com>
2020-07-22 13:47:42 -0300

    add new pages: projects and contact

---
+++ src/pages/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout :show-logo="true">
+    <!-- Author intro -->
+    <Author :show-title="true" />
+
+  </Layout>
+</template>
+
+<script>
+import Author from '~/components/Author.vue'
+
+export default {
+  components: {
+    Author
+  },
+  metaInfo: {
+    title: 'Contato'
+  }
+}
+</script>
